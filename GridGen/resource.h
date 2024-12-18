5eadd18034b297e504f9aa9473b8961416fc29ba
Benedict Lee <ben@parallelsuns.com>
2024-12-18 15:38:51 +0800

    rearrange folder structure

---
+++ GridGen/resource.h
@@ -0,0 +1,39 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by WindowsProject1.rc
+//
+#define IDC_MYICON                      2
+#define IDD_WINDOWSPROJECT1_DIALOG      102
+#define IDS_APP_TITLE                   103
+#define IDM_ABOUT                       104
+#define IDI_SMALL                       108
+#define IDC_WINDOWSPROJECT1             109
+#define IDR_MAINFRAME                   128
+#define IDD_CHOOSEGRIDSIZE              129
+#define IDD_MAINDIALOG                  129
+#define IDC_EDIT1                       1000
+#define IDC_CHOOSEGRIDSIZE_INPUT        1000
+#define IDC_CHOOSEFILES                 1001
+#define ID_SAVE                         1002
+#define ID_EXIT                         1003
+#define IDC_FILECOUNT                   1004
+#define IDC_MAXLENGTH                   1005
+#define IDC_CHANNELS                    1007
+#define IDC_COMBO1                      1008
+#define IDC_BITDEPTH                    1008
+#define IDC_SAMPLERATE                  1009
+#define ID_FILE_CHOOSEFILES             32771
+#define ID_FILE_CHOOSEGRIDSIZE          32772
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32775
+#define _APS_NEXT_CONTROL_VALUE         1009
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
